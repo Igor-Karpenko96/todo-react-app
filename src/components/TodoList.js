038af68ec0a59ed9be5eeb22f1f9bc26921dfa4a
Igor Karpenko <karpenkoigor961@gmail.com>
2021-01-11 21:37:14 +0200

    toDo-react-app

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToDo from './ToDo';
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <ToDo todos={todos} setTodos={setTodos} text={todo.text} key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
